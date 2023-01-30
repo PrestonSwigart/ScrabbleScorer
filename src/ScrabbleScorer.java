7dbd794389e134f94b3498d1d8c37828665a5212
Preston Swigart <prestonlswigart@gmail.com>
2023-01-30 10:19:18 -0500

    1.30.23 10:19

--- src/ScrabbleScorer.java
+++ src/ScrabbleScorer.java
@@ -1,5 +1,6 @@
 import java.io.File;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Scanner;
 
 public class ScrabbleScorer {
@@ -7,29 +8,60 @@ public class ScrabbleScorer {
     private int[] values = {1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10};
     private ArrayList<ArrayList<String>> dictionary;
 
+    /**
+     * constructor
+     */
     public ScrabbleScorer(){
         dictionary = new ArrayList<>();
         for(int i = 0; i < 26; i++)
             dictionary.add(new ArrayList<String>());
+        buildDictionary();
     }
 
+    /**
+     * builds dictionary
+     */
     public void buildDictionary(){
         try{
-            Scanner in = new Scanner(new File("SCRABBLEWORDS.txt"));
+            Scanner in = new Scanner(new File("SCRABBLE_WORDS.txt"));
             while(in.hasNext()){
-
+                String word = in.nextLine();
+                int index = alpha.indexOf(word.substring(0,1));
+                dictionary.get(index).add(word);
             }
             in.close();
+            for(int i = 0; i < dictionary.size(); i++){
+                Collections.sort(dictionary.get(i));
+            }
         }
         catch(Exception e){
             System.out.println("Error here: " + e);
         }
     }
 
+    /**
+     * checks if the word is valid
+     * @param word
+     * @return
+     */
     public boolean isValidWord(String word){
-        return true;
+        try {
+            dictionary.get(alpha.indexOf(word.substring(0, 1)));
+            if (Collections.binarySearch(dictionary.get(alpha.indexOf(word.substring(0, 1))), word) < 0) {
+                return false;
+            }
+            return true;
+        }
+        catch(Exception e)  {
+            return false;
+        }
     }
 
+    /**
+     * gets the word score
+     * @param word
+     * @return
+     */
     public int getWordScore(String word){
         int score = 0;
         for(int i = 0; i < word.length(); i++){
@@ -39,8 +71,26 @@ public class ScrabbleScorer {
         return score;
     }
 
+    /**
+     * main
+     * @param args
+     */
     public static void main(String[] args){
         ScrabbleScorer app = new ScrabbleScorer();
-
+        System.out.println("* Welcome to the Scrabble Word Scorer App *");
+        Scanner in = new Scanner(System.in);
+        while(true){
+            System.out.println("Enter a word to score or 0 to quit:");
+            String word = in.nextLine().toUpperCase();
+            if(word.equals("0")){
+                break;
+            }
+            if(app.isValidWord(word)){
+                System.out.println(word + " = " + app.getWordScore(word) + " points");
+            }
+            else{
+                System.out.println(word + " is not a valid word in the dictionary");
+            }
+        }
     }
 }
